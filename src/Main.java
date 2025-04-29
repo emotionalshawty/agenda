cf0af49bc9285a749f669fe778ed27cf7141f2d9
biomega <matrixomfg@gmail.com>
2025-04-29 13:21:32 +0200

    x

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,75 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends tui {
+    Scanner sc = new Scanner(System.in);
+    ArrayList<contacte> Contactes = new ArrayList<>();
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<String> Contactes = new ArrayList<>();
 
-        System.out.println("Benvolgut a la teva agenda, esscull una opció");
-        System.out.println("1. Crea un contacte.");
-        System.out.println("2. Busca un contacte existent.");
-        System.out.println("3. Actualitza un contacte existent.");
-        System.out.println("4. Esborra un contacte existent.");
-        System.out.println("5. Sortir de l'agenda.");
+        boolean cont = true;
 
-        int opcio = sc.nextInt();
 
-        System.out.println("Has escollit l'opció "+ opcio);
-        switch (opcio) {
-            case 1:
-                System.out.println("POsa el nom del contacte que vols crear:");
-                String nom = sc.next();
+        private void crearcontact    ( sc, ArrayList<contacte> Contactes){
+            System.out.println("POsa el nom del contacte que vols crear:");
+            String nom = sc.nextLine();
 
-                System.out.println("Posa el numero de telefon:");
-                String tel = sc.next();
+            System.out.println("Posa el numero de telefon:");
+            String tel = sc.nextLine();
 
-                System.out.println("Posa el email");
-                String email = sc.next();
-                Contactes.add("Nom:"+nom+"Telèfon:"+tel+"Email:"+email );
+            System.out.println("Posa el email");
+            String email = sc.nextLine();
+            contacte contactenou = new contacte(nom,tel,email);
+            Contactes.add(contactenou );
 
-                System.out.println("S'ha creat l'entrada de  " + nom);
-                break;
-            case 2:
-                System.out.println("Posa el nom del conrtacte que vols buscar:");
-                String buscar = sc.next();
-                if (Contactes.contains(buscar)) {
-                    System.out.println("S'ha trobat"+ buscar);
-                } else {
-                    System.out.println("No s'ha trobat res ");
-                }
-                break;
-            case 3:
-                System.out.println("posa el nom del contacte que vols actualitzar:");
-                String actu = sc.next();
-                if (Contactes.contains(actu)) {
-                    System.out.println("Posa el nou nom del contacte:");
-                    String nouNom = sc.next();
-                    Contactes.set(Contactes.indexOf(actu), nouNom);
-                    System.out.println("S'ha actualitzat el contacte a: " + nouNom);
-                } else {
-                    System.out.println("No s'ha trobat un contacte amb el nom " + actu );
-                }
-                break;
-            case 4:
-                System.out.println("Posa el nom del contacte que vols esborrar");
-                String esbor = sc.next();
-                if (Contactes.remove(esbor)) {
-                    System.out.println("s'ha esborrat " + esbor );
-                } else {
-                    System.out.println("No s'ha trobat un contacte amb aquest nom ");
-                }
-                break;
-            case 5:
-                System.out.println("Sortint...");
-                break;
-            default:
-                System.out.println("L'opció que has escollit no es valida, prova'n una altra");
+            System.out.println("S'ha creat l'entrada de contacte de " + nom);
         }
+        private void buscarcontacte( sc, ArrayList<contacte> Contactes){
+            System.out.println("Posa el nom del conrtacte que vols buscar:");
+            String buscar = sc.next();
+            if (Contactes.contains(buscar)) {
+                System.out.println("S'ha trobat"+ buscar);
+            } else {
+                System.out.println("No s'ha trobat res ");
+            }
+        }
+        private void actucontacte( sc, ArrayList<contacte> Contactes) {
+            System.out.println("posa el nom del contacte que vols actualitzar:");
+            String actu = sc.next();
+            if (Contactes.contains(actu)) {
+                System.out.println("Posa el nou nom del contacte:");
+                String nouNom = sc.next();
+                Contactes.set(Contactes.indexOf(actu), nouNom);
+                System.out.println("S'ha actualitzat el contacte a: " + nouNom);
+            } else {
+                System.out.println("No s'ha trobat un contacte amb el nom " + actu);
+            }
+        }
+        private void elimcontact( sc, ArrayList<contacte> Contactes) {
+            System.out.println("Posa el nom del contacte que vols esborrar");
+            String esbor = sc.next();
+            if (Contactes.remove(esbor)) {
+                System.out.println("s'ha esborrat " + esbor);
+            } else {
+                System.out.println("No s'ha trobat un contacte amb aquest nom ");
+            }
+        }
+        private void llistacontactes(sc, ArrayList<contacte> Contactes;) {
+            System.out.println("\n== LLISTAT DE CONTACTES ==");
+            ArrayList<contacte> contactes = gestor.getContactes();
+
+            if (contactes.isEmpty()) {
+                System.out.println("No hi ha contactes registrats.");
+                return;
+            }
+
+            for (int i = 0; i < contactes.size(); i++) {
+                System.out.println((i + 1) + ". " + contactes.get(i));
+            }
+        }
+
+    }
+
+
 
     }
 }
--- src/contacte.java
+++ src/contacte.java
@@ -1,2 +1,39 @@
 public class contacte {
+    String nom;
+    String tel;
+    String email;
+
+    public contacte(String nom, String tel, String email) {
+        this.nom = nom;
+        this.tel = tel;
+        this.email = email;
+    }
+
+    public String toString(){
+        return "Nom:"+nom+"Telèfon:"+tel+"Email:"+email;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
