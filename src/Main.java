f192386a05891816b5d77955794ec9c9c18eacd5
emotionalshawtea <matrixomfg@gmail.com>
2025-04-30 20:24:08 +0200

    1

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 
 public class Main {
-    Scanner sc = new Scanner(System.in);
-    private ArrayList<contacte> Contactes = new ArrayList<>();
+
 
     public static void main(String[] args) {
-        Main app = new Main();
+        control app = new control();
         tui tui = new tui();
         while (true) {
-            switch (tui.menu()) {
+            int opcio = tui.menu();
+            switch (opcio) {
                 case 1:
                     app.crearcontacte();
                     break;
@@ -27,110 +27,16 @@ public class Main {
                     app.llistacontactes();
                     break;
                 case 6:
-                    System.out.println("Sortint de l'aplicació...");
+                    System.out.println("Estàs sortint de l'aplicació");
                     System.exit(0);
                     break;
-                default: // Opció no vàlida
-                    System.out.println("Opció no vàlida.");
+                default:
+                    System.out.println("No has escollit una opció valida");
             }
         }
 
     }
 
 
-    public void crearcontacte() {
-        System.out.println("POsa el nom de la persona:");
-        String nom = sc.nextLine();
 
-        System.out.println("Introdueix el seu número de telèfon:");
-        String tel = sc.nextLine();
-
-        System.out.println("Quin correu electronic té? ");
-        String email = sc.nextLine();
-
-        contacte contactenou = new contacte(nom, tel, email);
-        Contactes.add(contactenou);
-
-        System.out.println("S'ha creat l'entrada de contacte de " + nom);
-    }
-
-    public void buscarcontacte() {
-        System.out.println("P0sa el nom del contacte que vols buscar");
-        String busca = sc.nextLine();
-
-        for (contacte trobat : Contactes) {
-            if (trobat.getNom().equalsIgnoreCase(busca)) {
-                System.out.println("S'ha trobat: " + trobat);
-                return;
-            }
-        }
-        System.out.println("No s'ha trobat a ninguna persona amb el nom de " + busca);
-    }
-
-    public void actucontact() {
-        System.out.println("Qiun es el nom del contacte que vols actualitzar?");
-        String actu = sc.nextLine();
-
-        for (contacte c : Contactes) {
-            if (c.getNom().equalsIgnoreCase(actu)) {
-
-                System.out.println("Quin camp vols actualitzar?");
-                System.out.println("1. Nom");
-                System.out.println("2. Telèfon");
-                System.out.println("3. Correu electrònic");
-                System.out.print("Selecciona una opció: ");
-                int quin = sc.nextInt();
-                switch (quin) {
-                    case 1 -> {
-                        System.out.println("Posa el nou nom");
-                        String nouNom = sc.nextLine();
-                        c.setNom(nouNom);
-                        System.out.println("S'ha actualitzat el nom a " + nouNom);
-                    }
-                    case 2 -> {
-                        System.out.println("Posa el nou # de telèfon:");
-                        String nouTel = sc.nextLine();
-                        c.setTel(nouTel);
-                        System.out.println("El nou telèfon es" + nouTel);
-                    }
-                    case 3 -> {
-                        System.out.println("Quin serà el nou correu electrònic?");
-                        String nouEmail = sc.nextLine();
-                        c.setEmail(nouEmail);
-                        System.out.println("S'ha actualitzat al seguent nou correu: " + nouEmail);
-                    }
-                    default -> System.out.println("Opció no vàlida.");
-                }
-                sc.nextLine();
-                return;
-            }
-        }
-        System.out.println("No s'ha trobat un contacte que es digui " + actu);
-    }
-
-    public void elimcontacte() {
-        System.out.println("Posa el nom del contacte que vols esborrar:");
-        String esbor = sc.nextLine();
-
-        for (contacte c : Contactes) {
-            if (c.getNom().equalsIgnoreCase(esbor)) {
-                Contactes.remove(c);
-                System.out.println("S'ha esborrat la entrada de " + esbor);
-                return;
-            }
-        }
-        System.out.println("No s'ha trobat un contacte amb aquest nom.");
-    }
-
-    public void llistacontactes() {
-        System.out.println("\n== LLISTAT DE CONTACTES ==");
-        if (Contactes.isEmpty()) {
-            System.out.println("No hi ha ningun contacte a la llista.");
-            return;
-        }
-
-        for (int i = 0; i < Contactes.size(); i++) {
-            System.out.println((i + 1) + ". " + Contactes.get(i));
-        }
-    }
 }
--- src/tui.java
+++ src/tui.java
@@ -10,14 +10,14 @@ public class tui {
     public int menu() {
         System.out.println("Benvingut a la gestió de contactes, que vols fer?");
         System.out.println("\n1. Crear contacte");
-            System.out.println("2. Buscar contacte");
-            System.out.println("3. Actualitzar contacte");
-            System.out.println("4. Eliminar contacte");
-            System.out.println("5. Llistar contactes");
-            System.out.println("6. Sortir");
-            System.out.print("Selecciona una opció: ");
-            int opcio = sc.nextInt();
-            sc.nextLine();
+        System.out.println("2. Buscar contacte");
+        System.out.println("3. Actualitzar contacte");
+        System.out.println("4. Eliminar contacte");
+        System.out.println("5. Llistar contactes");
+        System.out.println("6. Sortir");
+        System.out.print("Selecciona una opció: ");
+        int opcio = sc.nextInt();
+        sc.nextLine();
         return opcio;
     }
 
