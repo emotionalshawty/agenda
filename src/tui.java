70cade7802e4e1f58b03144ab556d643c281413f
emotionalshawtea <matrixomfg@gmail.com>
2025-04-30 20:30:54 +0200

    1

--- src/tui.java
+++ src/tui.java
@@ -8,12 +8,12 @@ public class tui {
     }
 
     public int menu() {
-        System.out.println("Benvingut a la gestió de contactes, que vols fer?");
+        System.out.println("Benvingut a la teva agenda de contactes, que vols fer?");
         System.out.println("\n1. Crear contacte");
         System.out.println("2. Buscar contacte");
         System.out.println("3. Actualitzar contacte");
         System.out.println("4. Eliminar contacte");
-        System.out.println("5. Llistar contactes");
+        System.out.println("5. Llistar els contactes");
         System.out.println("6. Sortir");
         System.out.print("Selecciona una opció: ");
         int opcio = sc.nextInt();
