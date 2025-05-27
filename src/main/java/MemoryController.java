47d497496e894f9b1d46095c0b87a22ae10e2654
emotionalshawtea <matrixomfg@gmail.com>
2025-05-27 15:32:45 +0200

    creo q ahora si

--- src/main/java/MemoryController.java
+++ src/main/java/MemoryController.java
@@ -12,6 +12,9 @@ public class MemoryController implements Controller {
         fileController = new FileController();
         contactes = fileController.loadAllContacts();
     }
+    public void resetForTesting() {
+        contactes = new HashMap<>();
+    }
 
     // Crea un nou contacte i el guarda
 
@@ -122,5 +125,48 @@ public class MemoryController implements Controller {
 
     public HashMap<Integer, Contacte> getContactes() {
         return contactes;
+    }
+    // prebas unitarias
+    public static void main(String[] args) {
+
+        MemoryController test = new MemoryController();
+
+        test.resetForTesting();
+
+        if (test.contactes == null) {
+            test.contactes = new HashMap<>();
+        }
+
+
+        System.out.println("Creando contacto Frodo...");
+        test.crearcontacte("Frodo", "Baggins", "3333333", "bolson@anillo.com");
+        System.out.println("se ha creado el contacto de "+ test.getContactes());
+        assert test.getContactes().size() == 1 : "Deberia de guardarse el contacto del portador del anillo. El ID que tiene es: " + test.getContactes().size();
+
+        // Buscar por nombre incorrecto, debe fallar
+        System.out.println("Buscando a Enrique (no existe)...");
+        Contacte c = test.buscarcontacte(1, "Enrique", -1);
+        System.out.println("Resultado de la búsqueda: " + c);
+        assert c == null : "No deberruia de encontrar ningún contacto llamado Enrique";
+
+        // Buscar por nombre correcto, debe encontrar a Frodo
+        System.out.println("Buscando a Frodo...");
+        c = test.buscarcontacte(1, "Frodo", -1);
+        System.out.println("se ha encontrado el contacto de "+ test.getContactes());
+        assert c != null && c.getNom().equalsIgnoreCase("Frodo") : "Deberia de encontrar a frodo";
+
+        System.out.println("Actualizando apellido de Frodo a Bolson...");
+        test.actucontact(1, "Frodo", -1, 2, "Bolson");
+        c = test.buscarcontacte(1, "Frodo", -1); // Se busca por el nombre original Frodo
+        System.out.println("se ha actualizado el contacto de "+ test.getContactes());
+        assert c != null && c.getCognom().equalsIgnoreCase("Bolson") : "Deberia de cambiarse su apellido a su apodo. Apellido actual: " + (c != null ? c.getCognom() : "null");
+
+        // Frodo ha muerto, hayq eue liminarle el contacto :(
+        System.out.println("Eliminando a Frodo...");
+        boolean deleted = test.elimcontacte(1, "Frodo", -1); // Se elimina buscando por Frodo
+        assert deleted : "Se ha borrado el contacto de Frodo";
+        assert test.getContactes().isEmpty() : "LOs contactos deberian de estar vacios. Tamaño actual: " + test.getContactes().size();
+
+        System.out.println("Se ha pasado todas las pruebas");
     }
 }
